a4f5412925e98f8fd4bd4ef6c3c9e76ee1d58076
Asteinch <andreassteinnes@gmail.com>
2023-11-17 00:34:24 +0100

    made non hostile | can reset

--- Code/Objects/bird.py
+++ Code/Objects/bird.py
@@ -1,9 +1,4 @@
-import pygame
-
-
-
-
-
+import pygame, random
 
 class Bird:
 
@@ -12,22 +7,36 @@ class Bird:
         self.imgs = [pygame.image.load("./assets/bird1.png"), pygame.image.load("./assets/bird2.png")]
         self.anim_index = 0
 
-        self.x, self.y = 500, 0
+        self.x, self.y = random.randint(1280, 2000), random.randint(0, 50)
 
         self.frames = 0
 
     def update(self):
 
+        self.x -=5
         self.animate_player()
 
     def animate_player(self):
 
         self.frames += 1
-
+        
         if self.frames == 10:
 
             self.anim_index = 0 if self.anim_index == 1 else 1
             self.frames = 0
+
+    def is_out_of_bounds(self):
+
+        if self.x < -200:
+
+            return True
+
+
+    def reset(self):
+
+        self.x = random.randint(1280, 2000)
+        self.y = random.randint(0, 50)
+
 
     def draw(self, win):
 
